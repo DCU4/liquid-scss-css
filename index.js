83c79699425a67b5ec8965ea9ebc2f65ce6c71fb
Dylan Connor <DylanJConnor4@gmail.com>
2022-11-06 20:49:00 -0500

    setup temp static files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 app.use(express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
+app.use(express.static(__dirname + '/temp'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true , limit: '50mb'}));
 
